3460409d7a6c80dbb3afe0715d2428a48dc84ef8
abhay <abhayv290@gmail.com>
2023-08-31 16:14:15 +0530

    Leetcode problems

---
+++ os.py
@@ -0,0 +1,12 @@
+# I wrote this program for clearing the exe files if you many of them 
+ # you just have to run progarm in the terminal and there you are
+
+
+
+import os
+files= os.listdir()
+for i in range(1,len(files)):
+    if(files[i].endswith('.exe')):
+        print(files[i])
+        os.remove(files[i])
+       
