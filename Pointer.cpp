30ce25a44b569af376ff088af0fc045cd6f5cee9
abhay <abhay290@gmail.com>
2023-09-29 01:48:13 +0530

    Pointers

---
+++ Pointer.cpp
@@ -0,0 +1,62 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+//     int num = 5;
+//     cout << num << endl;
+
+
+// ///Adress of operator
+
+//     int *p = &num;
+//     cout << p << endl;
+
+
+// //  size of operator
+
+//     cout << "Size of operator:" << sizeof(num);
+
+// // Copying a Pointer
+
+//     int *q = p;
+//     cout << q << " " << p << endl;
+
+//     cout << *q << " " << *p << endl;
+
+// // Playing with an array  with pointers
+
+//     int arr[20];
+//     cout << arr; // print the address of arr[20];
+//     cout << " " << &arr[0];  // also print the address of arr[20]
+//     cout << " " << *arr;  // prints the value at arr[0] it prints a garbage value cz we didn't give it to it
+
+//     int ar[10] = {2, 4, 6} ; // Asigned some value to it
+//     cout << endl<< *ar << " " << *ar + 1 << " " << *(ar + 1)<< endl;
+//     int i = 0;
+//     cout << i[ar]<<endl; // Also indicate index i in array same as ar[i];
+
+//     int *ptr = &ar[0];  // Through Pointer
+//     cout << sizeof(ptr) << " " << sizeof(*ptr) << endl;
+
+
+
+/// CHAR ARRAYS
+
+// char ch[6] = "Abhay";
+
+// cout << ch << endl;
+
+// char *c = &ch[0];
+// cout << c << " " << *c << endl;
+
+
+
+ 
+
+
+
+
+return 0;
+
+} 
